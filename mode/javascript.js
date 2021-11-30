b1c2561fd63b5a3766680ddc95f6b661dbc6b13e
raduc <rac119@ic.ac.uk>
2021-11-30 15:56:15 +0000

    Revert "Changed typename color"
    This reverts commit 47cf97062285ce44b5a5af964804318dd197699e.

--- mode/javascript.js
+++ mode/javascript.js
@@ -90,11 +90,11 @@ function mkJavaScript(parserConfig) {
       stream.skipToEnd();
       return ret("meta", "meta");
     } else if (ch == "#" && stream.eatWhile(wordRE)) {
-      return ret("variable", "typeName")
+      return ret("variable", "property")
     } else if (ch == "<" && stream.match("!--") ||
                (ch == "-" && stream.match("->") && !/\S/.test(stream.string.slice(0, stream.start)))) {
       stream.skipToEnd()
-      return ret("comment", "meta")
+      return ret("comment", "comment")
     } else if (isOperatorChar.test(ch)) {
       if (ch != ">" || !state.lexical || state.lexical.type != ">") {
         if (stream.eat("=")) {
@@ -146,7 +146,7 @@ function mkJavaScript(parserConfig) {
       }
       maybeEnd = (ch == "*");
     }
-    return ret("comment", "meta");
+    return ret("comment", "comment");
   }
 
   function tokenQuasi(stream, state) {
